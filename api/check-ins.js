afbd626bee402f6022ce0b823d0c6d103f9d2335
Wei-Ming <barry800414@gmail.com>
2017-12-28 00:05:48 +0800

    get all working state

--- api/check-ins.js
+++ api/check-ins.js
@@ -2,6 +2,13 @@
 const express = require("express");
 const router = express.Router();
 
+
+router.get('/working', async(req, res) => {
+    const db = req.db.collection('sessions');
+    const result = await db.find({ '_state.isWorking': true }).toArray();
+    res.send(result);
+})
+
 router.get('/:id', async(req, res) => {
     const db = req.db.collection('checkIns');
     const userId = req.params.id;
@@ -14,6 +21,7 @@ router.get('/', async(req, res) => {
     const result = await db.find().toArray();
     res.send(result);
 })
+
 
 
 module.exports = router;
