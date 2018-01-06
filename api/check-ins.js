ef239cb742161335cfea09a2286f570afb2dc04f
Wei-Ming <barry800414@gmail.com>
2018-01-06 01:52:15 +0000

    add parameter for searching working time

--- api/check-ins.js
+++ api/check-ins.js
@@ -10,10 +10,24 @@ router.get("/working", async (req, res) => {
 router.get("/:key", async (req, res) => {
   const users = req.db.collection("users");
   const urlKey = req.params.key;
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  // TODO: validation
+
   const user = await users.findOne({ urlKey });
   if (user) {
     const checkIns = req.db.collection("checkIns");
-    const result = await checkIns.find({ userId: user._id }).toArray();
+    const query = { userId: user._id };
+    if (startDate && endDate) {
+      Object.assign(query, {
+        startTime: {
+          $gte: new Date(startDate),
+          $lt: new Date(endDate),
+        },
+      });
+    }
+    const result = await checkIns.find(query).toArray();
     res.send(result);
   } else {
     res.send([]);
