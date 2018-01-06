5c0a68d133795d32676ea395cceea8a6d7237953
Yen-chi Chen <mark86092@gmail.com>
2018-01-06 12:40:27 +0800

    Handle middleware error

--- api/check-ins.js
+++ api/check-ins.js
@@ -1,43 +1,54 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/working", async (req, res) => {
-  const db = req.db.collection("sessions");
-  const result = await db.find({ "_state.isWorking": true }).toArray();
-  res.send(result);
-});
+const wrapper = fn => (req, res, next) => fn(req, res).catch(err => next(err));
 
-router.get("/:key", async (req, res) => {
-  const users = req.db.collection("users");
-  const urlKey = req.params.key;
-  const startDate = req.query.startDate;
-  const endDate = req.query.endDate;
-
-  // TODO: validation
-
-  const user = await users.findOne({ urlKey });
-  if (user) {
-    const checkIns = req.db.collection("checkIns");
-    const query = { userId: user._id };
-    if (startDate && endDate) {
-      Object.assign(query, {
-        startTime: {
-          $gte: new Date(startDate),
-          $lt: new Date(endDate),
-        },
-      });
-    }
-    const result = await checkIns.find(query).toArray();
+router.get(
+  "/working",
+  wrap(async (req, res) => {
+    const db = req.db.collection("sessions");
+    const result = await db.find({ "_state.isWorking": true }).toArray();
     res.send(result);
-  } else {
-    res.send([]);
-  }
-});
+  })
+);
 
-router.get("/", async (req, res) => {
-  const db = req.db.collection("checkIns");
-  const result = await db.find().toArray();
-  res.send(result);
-});
+router.get(
+  "/:key",
+  wrap(async (req, res) => {
+    const users = req.db.collection("users");
+    const urlKey = req.params.key;
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
+
+    // TODO: validation
+
+    const user = await users.findOne({ urlKey });
+    if (user) {
+      const checkIns = req.db.collection("checkIns");
+      const query = { userId: user._id };
+      if (startDate && endDate) {
+        Object.assign(query, {
+          startTime: {
+            $gte: new Date(startDate),
+            $lt: new Date(endDate),
+          },
+        });
+      }
+      const result = await checkIns.find(query).toArray();
+      res.send(result);
+    } else {
+      res.send([]);
+    }
+  })
+);
+
+router.get(
+  "/",
+  wrap(async (req, res) => {
+    const db = req.db.collection("checkIns");
+    const result = await db.find().toArray();
+    res.send(result);
+  })
+);
 
 module.exports = router;
