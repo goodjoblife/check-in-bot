05e07450502378f0b4708dded2354b7d50172a50
Wei-Ming <barry800414@gmail.com>
2018-01-14 17:25:32 +0000

    modify flows of conversation, add gps location, fix url problem

--- handlers/index.js
+++ handlers/index.js
@@ -10,6 +10,7 @@ const {
   formatTime,
   genQuickReply,
   genRandomReply,
+  resetState,
 } = require("../utils");
 const { prepareCheckIn } = require("../models");
 const {
@@ -41,6 +42,7 @@ const {
  * handlers 的順序有差，如果 handler 內沒有執行 terminate，就會一直往下判定，判定過了就會執行。
  *
  */
+// 這是一款協助你紀錄工時、計算加班費，同時製造無量功德的聊天機器人
 const handlers = [
   // handle get started
   {
@@ -48,11 +50,64 @@ const handlers = [
     handler: async (context, db, terminate) => {
       context.resetState();
       await context.sendText(
-        "歡迎使用功德無量打卡機，這是一款協助你紀錄工時、計算加班費，同時製造無量功德的聊天機器人。"
+        "嗨嗨你好，我是功德無量打卡機本人，請叫我阿德就好。",
+        genQuickReply([{ text: "你是誰? 你可以幹嘛?" }])
       );
+      terminate();
+    },
+  },
+  {
+    state: [{ seenTutorial: false }],
+    event: [{ text: "你是誰? 你可以幹嘛?" }],
+    handler: async (context, db, terminate) => {
       await context.sendText(
-        "馬上就來使用看看吧！",
-        genQuickReply([P.CHECK_IN])
+        "我是一個打卡機，我可以幫你：\n\n- 紀錄工時\n- 計算應該拿到的加班費\n\n還有... 製造滿滿的功德",
+        genQuickReply([{ text: "要怎麼使用？" }])
+      );
+      terminate();
+    },
+  },
+  {
+    state: [{ seenTutorial: false }],
+    event: [{ text: "要怎麼使用？" }],
+    handler: async (context, db, terminate) => {
+      await context.sendText(
+        "很簡單，按下「上班，做功德」，就會開始記錄你的工時，你的功德就會源源不絕地產生囉^^",
+        genQuickReply([{ text: "那要怎麼下班？" }])
+      );
+      terminate();
+    },
+  },
+  {
+    state: [{ seenTutorial: false }],
+    event: [{ text: "那要怎麼下班？" }],
+    handler: async (context, db, terminate) => {
+      await context.sendText(
+        "按下「下班，不做了」，就會結束記錄你的工時囉！",
+        genQuickReply([{ text: "就這樣？還有其他功能嗎？" }])
+      );
+      terminate();
+    },
+  },
+  {
+    state: [{ seenTutorial: false }],
+    event: [{ text: "就這樣？還有其他功能嗎？" }],
+    handler: async (context, db, terminate) => {
+      await context.sendText(
+        "當然有，你還可以：\n\n - 查看你的每一筆工時，而且加班費都幫你算好了喔\n - 查看今天台灣人已經累積多少功德\n - 上下班即時動態，看現在還有多少人在做功德",
+        genQuickReply([{ text: "好，我懂了，開始使用！" }])
+      );
+      terminate();
+    },
+  },
+  {
+    state: [{ seenTutorial: false }],
+    event: [{ text: "好，我懂了，開始使用！" }],
+    handler: async (context, db, terminate) => {
+      context.setState({ seenTutorial: true });
+      await context.sendText(
+        "太好了！ 馬上來做功德吧！",
+        genQuickReply([{ type: P.CHECK_IN }])
       );
       terminate();
     },
@@ -61,7 +116,7 @@ const handlers = [
   {
     state: [{ isWorking: false }],
     event: [
-      { text: ["做功德", "上班"] },
+      { text: ["做功德", "上班", "開始上班做功德"] },
       { postbackPayload: P.CHECK_IN },
       { payload: P.CHECK_IN },
     ],
@@ -71,12 +126,34 @@ const handlers = [
         "哈庫納罵踏踏！！ 你的功德正在源源不絕地產生中..."
       );
       await context.sendText(
-        "不如來看看其他人已經產生了多少功德？",
+        "不如再記錄下你的工作位置吧！",
+        genQuickReply([{ type: P.SEND_LOCATION }, { type: P.CHECK_OUT }])
+      );
+      terminate();
+    },
+  },
+  // handle user sending location
+  {
+    event: [{ hasLocation: true }],
+    handler: async (context, db, terminate) => {
+      const location = getLocation(context);
+      const locationTimestamp = getTimeStamp(context);
+      if (context.state.isWorking) {
+        context.setState({ location, locationTimestamp });
+      } else {
+        context.setState({
+          isWorking: true,
+          startTime: new Date(),
+          location,
+          locationTimestamp,
+        });
+      }
+      await context.sendText(
+        "打卡成功！\n\n不如...",
         genQuickReply([
-          P.VIEW_TOTAL_WORKING_TIME,
-          P.VIEW_WORKING_USER_COUNT,
-          P.VIEW_MY_WORKING_TIME,
-          P.CHECK_OUT,
+          { type: P.VIEW_TOTAL_WORKING_TIME, text: "來看看全台灣累積的功德數" },
+          { type: P.VIEW_WORKING_USER_COUNT, text: "來看看有多少人正在做功德" },
+          { type: P.CHECK_OUT },
         ])
       );
       terminate();
@@ -86,18 +163,14 @@ const handlers = [
   {
     state: [{ isWorking: true }],
     event: [
-      { text: ["做功德", "上班"] },
+      { text: ["做功德", "上班", "開始上班做功德"] },
       { postbackPayload: P.CHECK_IN },
       { payload: P.CHECK_IN },
     ],
     handler: async (context, db, terminate) => {
       await context.sendText(
-        "哎呀，你已經在做功德了啦！ 不如看看其他人做了多少功德吧！",
-        genQuickReply([
-          P.VIEW_TOTAL_WORKING_TIME,
-          P.VIEW_WORKING_USER_COUNT,
-          P.CHECK_OUT,
-        ])
+        "哎呀，你已經在做功德了啦！ 不如再記錄下你的工作位置吧！",
+        genQuickReply([{ type: P.SEND_LOCATION }, { type: P.CHECK_OUT }])
       );
       terminate();
     },
@@ -106,14 +179,14 @@ const handlers = [
   {
     state: [{ isWorking: false }],
     event: [
-      { text: ["不做了", "下班"] },
+      { text: ["不做了", "下班", "下班了，不做了"] },
       { postbackPayload: P.CHECK_OUT },
       { payload: P.CHECK_OUT },
     ],
     handler: async (context, db, terminate) => {
       await context.sendText(
         "哎呀，其實你今天還沒開始做功德啦，現在馬上開始做吧！",
-        genQuickReply([P.CHECK_IN])
+        genQuickReply([{ type: P.CHECK_IN }])
       );
       terminate();
     },
@@ -145,15 +218,15 @@ const handlers = [
       await context.sendText(
         "台灣因為有你的功德，才能有今日亮眼的經濟成績。\n\n善哉善哉，讚嘆、感恩施主。",
         genQuickReply([
-          P.VIEW_MY_WORKING_TIME,
-          P.VIEW_WORKING_USER_COUNT,
-          P.VIEW_TOTAL_WORKING_TIME,
-          P.CHECK_IN,
+          { type: P.VIEW_MY_WORKING_TIME },
+          { type: P.VIEW_WORKING_USER_COUNT },
+          { type: P.VIEW_TOTAL_WORKING_TIME },
+          { type: P.CHECK_IN },
         ])
       );
 
       // reset state
-      context.resetState();
+      resetState(context);
       terminate();
     },
   },
@@ -166,9 +239,14 @@ const handlers = [
     handler: async (context, db, terminate) => {
       const userId = context._session._id;
       const urlKey = await findOrCreateUserUrlKey(db, userId);
-      const url = `${FRONTEND_URL}/#/check-ins}/${urlKey}`;
-      const qrPayloads = [P.VIEW_TOTAL_WORKING_TIME, P.VIEW_WORKING_USER_COUNT];
-      qrPayloads.push(context.state.isWorking ? P.CHECK_OUT : P.CHECK_IN);
+      const url = `${FRONTEND_URL}/#/check-ins/${urlKey}`;
+      const qrPayloads = [
+        { type: P.VIEW_TOTAL_WORKING_TIME },
+        { type: P.VIEW_WORKING_USER_COUNT },
+      ];
+      qrPayloads.push(
+        context.state.isWorking ? { type: P.CHECK_OUT } : { type: P.CHECK_IN }
+      );
       await context.sendText(
         "你製造的功德都在這了，應該拿到的加班費也幫你算好了！"
       );
@@ -190,8 +268,10 @@ const handlers = [
       const userId = context._session._id;
       const urlKey = await findOrCreateUserUrlKey(db, userId);
       const url = FRONTEND_URL;
-      const qrPayloads = [P.VIEW_WORKING_USER_COUNT, P.VIEW_MY_WORKING_TIME];
-      qrPayloads.push(context.state.isWorking ? P.CHECK_OUT : P.CHECK_IN);
+      const qrPayloads = [{ type: P.VIEW_WORKING_USER_COUNT }];
+      qrPayloads.push(
+        context.state.isWorking ? { type: P.CHECK_OUT } : { type: P.CHECK_IN }
+      );
       await context.sendText(
         "想要全台灣勞工的累積的功德嗎？想要的話可以全部給你，去找吧！我把所有的功德都放在那裡了。"
       );
@@ -210,24 +290,25 @@ const handlers = [
     handler: async (context, db, terminate) => {
       const { count, total } = await getWorkingUserCount(db);
       const percent = Math.round(count / total * 100);
-      if (percent > 30) {
+      if (percent > 50) {
         await context.sendText(
-          `哇！ 現在還有 ${percent}% 的使用者和你一起在做功德呢！ 你並不孤單喔！`
+          `哇！ 現在還有 ${count}位（${percent} %）的使用者和你一起在做功德呢！ 你並不孤單喔！`
         );
         await context.sendText(
           '為了台灣的經濟，犧牲奉獻一點，做點功德，沒關係的啦^^"',
-          genQuickReply([P.VIEW_MY_WORKING_TIME, P.CHECK_OUT])
+          genQuickReply([{ type: P.CHECK_OUT }])
         );
       } else {
         await context.sendText(
-          `呃...好像只剩 ${percent}% 的使用者還在做功德耶。`
+          `已經有${total - count}位（${100 -
+            percent} %）的使用者下班了。你怎麼還不下班？`
         );
         await context.sendText(
           "也沒關係啦，俗話說一個便當吃不飽，可以吃兩個。\n\n做一點功德還不夠，可以做兩點呀！"
         );
         await context.sendText(
           `樂觀一點想，還有 ${percent}% 的使用者在陪你做功德嘛`,
-          genQuickReply([P.VIEW_MY_WORKING_TIME, P.CHECK_OUT])
+          genQuickReply([{ type: P.CHECK_OUT }])
         );
       }
       terminate();
@@ -236,7 +317,7 @@ const handlers = [
   {
     state: [{ isWorking: false }],
     event: [
-      { text: ["看看還有多少人在做功德"] },
+      { text: ["看看多少人在做功德"] },
       { postbackPayload: P.VIEW_WORKING_USER_COUNT },
       { payload: P.VIEW_WORKING_USER_COUNT },
     ],
@@ -244,19 +325,29 @@ const handlers = [
       const { count, total } = await getWorkingUserCount(db);
       const percent = Math.round(count / total * 100);
       if (percent > 30) {
-        await context.sendText(`哇！ 現在還有 ${percent}% 的使用者在做功德...`);
         await context.sendText(
-          '本打卡機覺得台灣真是個寶島，勞工為國家的經濟發展犧牲奉獻，GDP卻越分越少，簡直便宜大碗又好用，這種地方哪裡找呀^^"'
+          `哇！ 現在還有 ${count}位（${percent} %）的使用者在做功德...`
+        );
+        await context.sendText(
+          '覺得台灣真是個寶島，勞工為國家的經濟發展犧牲奉獻，GDP卻越分越少，簡直便宜大碗又好用，這種地方哪裡找呀^^"'
         );
         await context.sendText(
           "明天記得也要上班做功德喔 ^＿^",
-          genQuickReply([P.VIEW_MY_WORKING_TIME, P.CHECK_IN])
+          genQuickReply([
+            { type: P.VIEW_MY_WORKING_TIME },
+            { type: P.CHECK_IN },
+          ])
         );
       } else {
-        await context.sendText(`只剩 ${percent}% 的使用者還在做功德了`);
+        await context.sendText(
+          `只剩 ${count}位（${percent} %）的使用者還在做功德了`
+        );
         await context.sendText(
           "明天記得也要上班做功德，讓台灣功德滿滿，台灣萬歲 ^O^！",
-          genQuickReply([P.VIEW_MY_WORKING_TIME, P.CHECK_IN])
+          genQuickReply([
+            { type: P.VIEW_MY_WORKING_TIME },
+            { type: P.CHECK_IN },
+          ])
         );
       }
       terminate();
@@ -312,6 +403,7 @@ const getPayloadFromContext = context => {
     : null;
   const payload = context.event.isPayload ? context.event.payload : null;
   const text = context.event.isText ? context.event.text : null;
+  const hasLocation = context.event.isLocation && context.event.hasAttachment;
   /* temporally not in use
   const location =
     context.event.isLocation && context.event.hasAttachment
@@ -321,6 +413,7 @@ const getPayloadFromContext = context => {
     context.event.isLocation && context.event.hasAttachment
       ? getTimeStamp(context)
       : null;
+
   const imgUrl =
     context.event.isImage && context.event.hasAttachment
       ? getImageUrl(context)
@@ -331,7 +424,7 @@ const getPayloadFromContext = context => {
     text,
   };
   */
-  return { postbackPayload, payload, text };
+  return { postbackPayload, payload, text, hasLocation };
 };
 
 const mainHandler = db => async context => {
