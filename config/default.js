9ec0bcde3a6ac437b4a70c0628b477da1d4388c4
Yen-chi Chen <mark86092@gmail.com>
2018-01-20 18:58:52 +0800

    加上 config 設定

--- config/default.js
+++ config/default.js
@@ -6,4 +6,5 @@ module.exports = {
   APP_ID: process.env.appId,
   APP_SECRET: process.env.appSecret,
   FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+  CHATBASE_API_KEY: process.env.CHATBASE_API_KEY,
 };
